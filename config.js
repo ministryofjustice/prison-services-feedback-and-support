e10b119761740db04b09c2ac7132bd0df7acd46c
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2020-12-08 16:55:05 +0000

    pat-1508 Adding Pathfinder support links and content update

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ module.exports = {
   urls: {
     oauth: process.env.OAUTH_ENDPOINT_URL || 'http://localhost:9090/auth/',
     allocationManager: process.env.ALLOCATION_MANAGER_ENDPOINT_URL || '',
+    pathfinder: process.env.PATHFINDER_UI_URL || 'http://localhost:3001/',
   },
 }
