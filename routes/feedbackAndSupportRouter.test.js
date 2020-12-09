ef20b3b268a8b0ffb3b8c59a1513442331c4551b
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2020-12-09 11:10:46 +0000

    pat-1508 removing router test as seems a bit overboard

--- routes/feedbackAndSupportRouter.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest')
-const app = require('../app')
-
-it('should render the Pathfinder page with a link to Pathfinder support', async () => {
-  const res = await request(app).get('/feedback-and-support/pathfinder')
-  expect(res.statusCode).toEqual(200)
-  expect(res.text).toContain('http://localhost:3001/useful-links/support')
-})
