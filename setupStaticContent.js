40535149cea1e1ff67748601474064076bd4ac30
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2024-01-04 14:34:26 +0000

    NN-5763: Update govuk-frontend (#143)

--- setupStaticContent.js
+++ setupStaticContent.js
@@ -6,7 +6,7 @@ const router = express.Router()
 module.exports = () => {
   router.use(express.static(path.join(__dirname, 'build/static')))
 
-  const assetPaths = ['./node_modules/govuk-frontend/govuk/assets', './node_modules/govuk-frontend']
+  const assetPaths = ['./node_modules/govuk-frontend/dist/govuk/assets', './node_modules/govuk-frontend/dist']
 
   assetPaths.forEach((dir) => {
     router.use('/assets', express.static(path.join(__dirname, dir)))
